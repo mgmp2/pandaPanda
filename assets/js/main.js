fa9e1c9a43aca4fc42839561e39ce6c89ce9816d
Miriam Mendoza <miriammp1997@gmail.com>
2017-04-18 11:31:53 -0500

    refactory code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,33 @@
 window.addEventListener("load", function (e){
   e.preventDefault();
-  var img = document.getElementsByClassName("js-panda");
-  var box= document.getElementsByClassName("box-photo");
 
-  createSpan(img,box);
-
-    // document.getElementById('body').classList.add("overflow-hidden");
-    // y = window.pageYOffset;
-
+  createSpan(document.getElementsByClassName("js-panda"),document.getElementsByClassName("box-photo"));
 })
-
 function createSpan(img,box){
   for (var i = 0; i < img.length; i++) {
     var span = document.createElement("span");
     var text = document.createTextNode("x");
     span.appendChild(text);
     span.addEventListener("click", function (e){
-      // e.target.style=visibility;
       e.target.classList.add("remove");
-e.target.previousElementSibling.classList.add("remove");
-
+      e.target.previousElementSibling.classList.add("remove");
    });
     box[i].appendChild(span);
 
   }
 }
+// SHOW/HIDDEN TEXT
+document.getElementById("origin").addEventListener("click", function(e){
+  e.preventDefault();
+  var hide = e.target.parentNode.nextElementSibling.firstElementChild;
+  hide.classList.toggle("visibility");
+});
+
+document.getElementById("history").addEventListener("click", function(e){
+  e.preventDefault();
+  e.target.parentNode.nextElementSibling.lastElementChild.classList.toggle("visibility");
+});
+// SHOW ALL IMG
 document.getElementById("restaurar").addEventListener("click", function (e){
   e.preventDefault();
   var children = e.target.parentNode.previousElementSibling.children;
@@ -32,17 +35,4 @@ document.getElementById("restaurar").addEventListener("click", function (e){
     children[i].firstElementChild.classList.remove("remove");
     children[i].lastElementChild.classList.remove("remove");
   }
-});
-document.getElementById("origin").addEventListener("click", function(e){
-  e.preventDefault();
-  var hide = e.target.parentNode.nextElementSibling.firstElementChild;
-  hide.classList.toggle("visibility");
-  console.log();
-});
-
-document.getElementById("history").addEventListener("click", function(e){
-  e.preventDefault();
-  var hide = e.target.parentNode.nextElementSibling.lastElementChild;
-  hide.classList.toggle("visibility");
-  console.log();
 });
